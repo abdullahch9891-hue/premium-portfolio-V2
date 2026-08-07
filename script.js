41d0ebb65e810da7df47a51697ce73b97c4ed2bb
abdullahch9891-hue <abdullah.ch9891@gmail.com>
2026-08-07 11:07:08 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,140 @@
+
+const hiddenElements = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+hiddenElements.forEach((el) => {
+    el.classList.add("hidden");
+    observer.observe(el);
+});
 const texts = [
-  "Web Developer",
-  "Frontend Developer",
-  "HTML & CSS Expert"
+    "Web Developer",
+    "Frontend Developer",
+    "HTML & CSS Expert"
 ];
 
-let index = 0;
-const heading = document.querySelector(".hero-text h2");
+const typing = document.getElementById("typing");
 
-setInterval(() => {
-    index = (index + 1) % texts.length;
-    heading.textContent = texts[index];
-}, 2000);
+let textIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+    const currentText = texts[textIndex];
+
+    if (!deleting) {
+        typing.textContent = currentText.substring(0, charIndex + 1);
+        charIndex++;
+
+        if (charIndex === currentText.length) {
+            deleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+    } else {
+        typing.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+
+        if (charIndex === 0) {
+            deleting = false;
+            textIndex = (textIndex + 1) % texts.length;
+        }
+    }
+
+    setTimeout(typeEffect, deleting ? 70 : 120);
+}
+
+window.onload = typeEffect;
+console.log(typeof particlesJS);
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#38bdf8"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      enable: true,
+      speed: 2
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#38bdf8",
+      opacity: 0.4,
+      width: 1
+    }
+  }
+});
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
+window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progress-bar").style.width = progress + "%";
+});
+const sections = document.querySelectorAll("section, header");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+const skillBars = document.querySelectorAll(".progress-bar");
+
+const skillsObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("animate");
+        }
+    });
+});
+
+skillBars.forEach((bar) => {
+    skillsObserver.observe(bar);
+});
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () => {
+document.querySelector(".nav-links").classList.toggle("show-menu");
+})  
